dda4adb4fc744f6ef21213c97a870083b71b5d48
CharlyssaG <cgulledge@kidkare.com>
2026-04-19 15:52:38 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BubbleChain from './BubbleChain.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BubbleChain />
-  </React.StrictMode>,
-)
